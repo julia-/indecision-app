1fef372a3f3b71680a58855324e34002995d283c
julia- <julia.ryanx@gmail.com>
2018-01-04 22:28:21 +1100

    add defaultProps for title and conditional render for subtitle in Header component

--- src/app.js
+++ src/app.js
@@ -40,12 +40,11 @@ class IndecisionApp extends React.Component {
   }
 
   render() {
-    const title = 'Indecision App'
     const subtitle = 'Put your life in the hands of a computer'
 
     return (
       <div>
-        <Header title={title} subtitle={subtitle} />
+        <Header subtitle={subtitle} />
         <Action
           hasOptions={this.state.options.length > 0}
           handlePick={this.handlePick}
@@ -64,9 +63,13 @@ const Header = props => {
   return (
     <div>
       <h1>{props.title}</h1>
-      <h2>{props.subtitle}</h2>
+      {props.subtitle && <h2>{props.subtitle}</h2>}
     </div>
   )
+}
+
+Header.defaultProps = {
+  title: 'Indecision App'
 }
 
 const Action = props => {
