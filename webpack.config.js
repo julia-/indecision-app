21df5aae5e07eb2801dcaf8a685758331013a645
julia- <julia.ryanx@gmail.com>
2018-01-05 19:39:32 +1100

    set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+// console.log(__dirname) // gets the absolute path
+
+module.exports = {
+  // tells webpack where it should start
+  entry: './src/app.js',
+  output: {
+    // need to provide an absolute path
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  }
+}
