112e4452e7875a1d9b237d085e17fca05b822697
julia- <julia.ryanx@gmail.com>
2018-01-06 02:05:17 +1100

    update components to implicitly return

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,14 +1,12 @@
 import React from 'react'
 
-const Option = props => {
-  return (
-    <div>
-      <li key={props.index}>{props.optionText}</li>
-      <button onClick={e => props.handleDeleteOption(props.optionText)}>
-        Remove
-      </button>
-    </div>
-  )
-}
+const Option = props => (
+  <div>
+    <li key={props.index}>{props.optionText}</li>
+    <button onClick={e => props.handleDeleteOption(props.optionText)}>
+      Remove
+    </button>
+  </div>
+)
 
 export default Option
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,30 +1,28 @@
 import React from 'react'
 import Option from './Option'
 
-const Options = props => {
-  return (
-    <div>
-      <h2>Options</h2>
-      {props.options.length > 0 ? (
+const Options = props => (
+  <div>
+    <h2>Options</h2>
+    {props.options.length > 0 ? (
+      <div>
+        <button onClick={props.handleDeleteAllOptions}>Remove options</button>
         <div>
-          <button onClick={props.handleDeleteAllOptions}>Remove options</button>
-          <div>
-            <ol>
-              {props.options.map((option, index) => (
-                <Option
-                  key={index}
-                  optionText={option}
-                  handleDeleteOption={props.handleDeleteOption}
-                />
-              ))}
-            </ol>
-          </div>
+          <ol>
+            {props.options.map((option, index) => (
+              <Option
+                key={index}
+                optionText={option}
+                handleDeleteOption={props.handleDeleteOption}
+              />
+            ))}
+          </ol>
         </div>
-      ) : (
-        <p>No options added. Add an option!</p>
-      )}
-    </div>
-  )
-}
+      </div>
+    ) : (
+      <p>No options added. Add an option!</p>
+    )}
+  </div>
+)
 
 export default Options
