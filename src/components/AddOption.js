6c16883ebac3383122307ca64c8c92ea428b3017
julia- <julia.ryanx@gmail.com>
2018-01-05 22:50:18 +1100

    create AddOption component to allow the user to add new options

---
+++ src/components/AddOption.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+class AddOption extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleAddOption = this.handleAddOption.bind(this)
+    this.state = {
+      error: undefined
+    }
+  }
+
+  handleAddOption(e) {
+    e.preventDefault()
+    const option = e.target.elements.option.value.trim()
+    const error = this.props.handleAddOption(option)
+
+    this.setState(() => ({ error }))
+
+    // clear user input if there is no error
+    if (!error) {
+      e.target.elements.option.value = ''
+    }
+  }
+  render() {
+    return (
+      <div>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option" />
+          <button>Add option</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default AddOption
