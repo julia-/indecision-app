dd14e0bb0482f60f37002119abbca199de30764f
julia- <julia.ryanx@gmail.com>
2018-01-06 23:17:59 +1100

    apply styles to AddOption component

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -5,7 +5,7 @@ class AddOption extends React.Component {
     error: undefined
   }
 
-  handleAddOption = (e) => {
+  handleAddOption = e => {
     e.preventDefault()
     const option = e.target.elements.option.value.trim()
     const error = this.props.handleAddOption(option)
@@ -20,9 +20,11 @@ class AddOption extends React.Component {
   render() {
     return (
       <div>
-        {this.state.error && <p>{this.state.error}</p>}
-        <form onSubmit={this.handleAddOption}>
-          <input type="text" name="option" />
+        {this.state.error && (
+          <p className="add-option-error">{this.state.error}</p>
+        )}
+        <form className="add-option" onSubmit={this.handleAddOption}>
+          <input className="add-option__input" type="text" name="option" />
           <button className="button">Add option</button>
         </form>
       </div>
