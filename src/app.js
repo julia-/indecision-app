300a8f8ebed820170499e64b82c939fbf3e6a4c6
julia- <julia.ryanx@gmail.com>
2018-01-06 18:31:06 +1100

    rearrange import order

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'normalize.css/normalize.css'
+
+import './styles/styles.scss'
 
 import IndecisionApp from './components/IndecisionApp'
-
-import 'normalize.css/normalize.css'
-import './styles/styles.scss'
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
